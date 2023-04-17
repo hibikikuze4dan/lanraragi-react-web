82aa4f876cb2982f17fb43b29c0a557b051d8721
Hibiki Kuze <desu2dan@gmail.com>
2023-04-16 23:13:41 -0500

    lint fix

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { useMediaQuery, useTheme } from "@mui/material";
-import { SideNavbar } from "./fragments/side-navbar";
 import { BottomNavbar } from "./fragments/bottom-navbar";
 
-export const Navbar = () => {
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("sm"));
-
-  return (
-    <>
-      {/* {matches && <SideNavbar />}
+export const Navbar = () => (
+  <>
+    {/* {matches && <SideNavbar />}
       {!matches && <BottomNavbar />} */}
-      <BottomNavbar />
-    </>
-  );
-};
+    <BottomNavbar />
+  </>
+);
