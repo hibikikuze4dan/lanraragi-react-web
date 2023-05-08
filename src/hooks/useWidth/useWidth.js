8a55be72df961cdc7142b257c938ad8329568f44
Hibiki Kuze <desu2dan@gmail.com>
2023-05-08 15:15:38 -0500

    add useWidth hook

---
+++ src/hooks/useWidth/useWidth.js
@@ -0,0 +1,20 @@
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+/**
+ * Be careful using this hook. It only works because the number of
+ * breakpoints in theme is static. It will break once you change the number of
+ * breakpoints. See https://legacy.reactjs.org/docs/hooks-rules.html#only-call-hooks-at-the-top-level
+ */
+export function useWidth() {
+  const theme = useTheme();
+  const keys = [...theme.breakpoints.keys].reverse();
+  return (
+    keys.reduce((output, key) => {
+      const matches = useMediaQuery(theme.breakpoints.only(key));
+      return !output && matches ? key : output;
+    }, null) || "xs"
+  );
+}
+
+export default useWidth;
