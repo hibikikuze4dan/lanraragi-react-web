e50c431e3aeb63297bbb91cc30757177cd71c0fa
Hibiki Kuze <desu2dan@gmail.com>
2023-05-12 17:59:58 -0500

    remove lodash

--- src/requests/random.js
+++ src/requests/random.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import throttle from "lodash/throttle";
 import { HEADERS, RANDOM_URL } from "./constants";
 import { getApiKey, getBaseUrl } from "../storage/requests";
 
@@ -8,20 +7,17 @@ const config = {
   headers: HEADERS,
 };
 
-export const getRandomArchives = throttle(
-  async (count = 10) =>
-    axios({
-      ...config,
-      headers: {
-        Authorization: `Bearer ${getApiKey()}`,
-      },
-      url: `http://${getBaseUrl()}${RANDOM_URL}?count=${count}`,
-    })
-      .then((response) => response.data.data)
-      .catch((error) => {
-        console.log(error);
-      }),
-  2000
-);
+export const getRandomArchives = async (count = 10) =>
+  axios({
+    ...config,
+    headers: {
+      Authorization: `Bearer ${getApiKey()}`,
+    },
+    url: `http://${getBaseUrl()}${RANDOM_URL}?count=${count}`,
+  })
+    .then((response) => response.data.data)
+    .catch((error) => {
+      console.log(error);
+    });
 
 export default getRandomArchives;
