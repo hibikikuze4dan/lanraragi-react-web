fc633ce81976b147f792093bb3d37ffc1d6a9cb8
Hibiki Kuze <desu2dan@gmail.com>
2023-05-13 11:28:33 -0500

    fix value not showing in search field

--- src/components/search-form/fragments/search-filter-text-field.js
+++ src/components/search-form/fragments/search-filter-text-field.js
@@ -1,22 +1,29 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { debounce } from "lodash";
 import { TextField } from "@mui/material";
 import { updateSearchFilter } from "../../../app/slice";
+import { getSearchFilter } from "../../../app/selectors";
 
 export const SearchFilterTextField = () => {
   const dispatch = useDispatch();
+  const [value, setValue] = useState(useSelector(getSearchFilter));
 
   const onChangeText = debounce((e) => {
     dispatch(updateSearchFilter(e.target.value));
   }, 250);
+  const onChange = (e) => {
+    setValue(e.target.value);
+    onChangeText(e);
+  };
 
   return (
     <TextField
       label="Search Filter"
       placeholder="Search Title, Artist, Series, Language or Tags"
       fullWidth
-      onChange={onChangeText}
+      value={value}
+      onChange={onChange}
       type="text"
       sx={{ mb: "1rem" }}
     />
