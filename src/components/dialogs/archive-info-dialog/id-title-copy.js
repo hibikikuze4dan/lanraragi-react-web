3dc6e1f5975840833aa105b48f333a17911d2904
Hibiki Kuze <desu2dan@gmail.com>
2023-09-26 18:23:23 -0500

    use typography instead of button for title and id so user can highlight to copy instead of using button due to permission issues

--- src/components/dialogs/archive-info-dialog/id-title-copy.js
+++ src/components/dialogs/archive-info-dialog/id-title-copy.js
@@ -1,30 +1,13 @@
 /* eslint-disable arrow-body-style */
 import React, { useCallback, useState } from "react";
-import { Button, Grid, IconButton, Typography } from "@mui/material";
-import { CopyAll } from "@mui/icons-material";
+import { Grid, IconButton, Typography } from "@mui/material";
+import { AbcOutlined, Pin } from "@mui/icons-material";
 
 export const IdTitleCopyButton = ({ arcId, archiveTitle }) => {
   const [showId, setShowId] = useState(false);
-  const onTitleClick = useCallback(() => {
+  const onSwitch = useCallback(() => {
     setShowId(!showId);
   }, [showId]);
-
-  const onCopyIconButtonClick = async () => {
-    try {
-      const permissions = await navigator.permissions.query({
-        name: "clipboard-write",
-      });
-      if (permissions.state === "granted" || permissions.state === "prompt") {
-        await navigator.clipboard.writeText(showId ? arcId : archiveTitle);
-      } else {
-        throw new Error(
-          "Can't access the clipboard. Check your browser permissions."
-        );
-      }
-    } catch (error) {
-      console.log("Error copying to clipboard:", error);
-    }
-  };
 
   return (
     <Grid
@@ -34,34 +17,25 @@ export const IdTitleCopyButton = ({ arcId, archiveTitle }) => {
       sx={{ height: "100%" }}
     >
       <Grid item xs={10}>
-        <Button
-          variant="text"
-          sx={{ textTransform: "none" }}
-          onClick={onTitleClick}
-          fullWidth
-        >
-          <Typography textAlign="center">
-            {showId ? (
-              <>
-                Archive ID:
-                <br />
-                <span style={{ overflowWrap: "anywhere" }}>{arcId}</span>
-              </>
-            ) : (
-              archiveTitle
-            )}
-          </Typography>
-        </Button>
+        <Typography textAlign="center">
+          {showId ? (
+            <>
+              Archive ID:
+              <br />
+              <span style={{ overflowWrap: "anywhere" }}>{arcId}</span>
+            </>
+          ) : (
+            archiveTitle
+          )}
+        </Typography>
       </Grid>
       <Grid item xs={2}>
         <IconButton
-          aria-label={`Click here to copy Archive ${
-            showId ? "ID" : "Title"
-          } to clipboard`}
-          onClick={onCopyIconButtonClick}
+          aria-label={`Click here to show Archive ${!showId ? "ID" : "Title"}`}
+          onClick={onSwitch}
           style={{ width: "100%", height: "100%", borderRadius: "unset" }}
         >
-          <CopyAll />
+          {showId ? <AbcOutlined /> : <Pin />}
         </IconButton>
       </Grid>
     </Grid>
